0f93f9ade5ad19e7c9e0e1b6bf5f7174f6f1b899
leviathon1432 <51034215+leviathon1618@users.noreply.github.com>
2020-03-04 12:42:44 +1300

    first commit

---
+++ Assets/SpellCombo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellCombo : MonoBehaviour
+{
+    public Dropdown element;
+    public Dropdown trajectory;
+    public Slider distance;
+    public Text output;
+    private float dist_val;
+
+    private int traj_item;
+    private string traj_choice;
+
+    private int element_item;
+    private string element_choice;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        output.text = "element: " + element_choice + " distance: " + dist_val.ToString() + " trajectory: " +  traj_choice;
+        dist_val = distance.value;
+
+        element_item = element.value;
+        element_choice = element.options[element_item].text;
+
+        traj_item = trajectory.value;
+        traj_choice = trajectory.options[traj_item].text;
+        
+    }
+}
