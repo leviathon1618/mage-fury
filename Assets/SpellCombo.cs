950f945e84d0b234f75352b09f5f17bee6542e31
leviathon1432 <51034215+leviathon1618@users.noreply.github.com>
2020-03-04 21:24:04 +1300

    made sphere inherit values from list
    the spell will change based on the inputs from the player

--- Assets/SpellCombo.cs
+++ Assets/SpellCombo.cs
@@ -5,34 +5,91 @@ using UnityEngine.UI;
 
 public class SpellCombo : MonoBehaviour
 {
+    
     public Dropdown element;
-    public Dropdown trajectory;
-    public Slider distance;
-    public Text output;
-    private float dist_val;
+    public Dropdown spell_path;
+    public Slider range;
+    public Slider width;
+    public Slider damage;
+    public Toggle explosion;
+    public Slider expl_dmg;
+    public Slider expl_range;
+    
 
-    private int traj_item;
-    private string traj_choice;
 
     private int element_item;
     private string element_choice;
+
+    private int spell_item;
+    private string traj_choice;
+
+    private float range_val;
+    private float width_val;
+    private float damage_val;
+    private bool explosion_state;
+    private float expl_dmg_val;
+    private float expl_range_val;
+
+    private string spell_features;
+
+    
+
+
+    public List<string> spell_properties = new List<string>();
+    private void Awake()
+    {
+        //spell_properties.Add("empty");
+    }
     // Start is called before the first frame update
     void Start()
     {
-       
+        
+        //print(spell_properties[0]);
+    }
+
+    public void spellGenerator()
+    {
+        spell_features = element_choice+
+            " "+ traj_choice+
+            " "+range_val.ToString()+
+            " "+width_val.ToString()+
+            " "+damage_val.ToString()+
+            " "+explosion_state.ToString()+
+            " "+expl_dmg_val.ToString()+
+            " "+ expl_range_val.ToString() ;
+        
+        spell_properties.Add(spell_features);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        output.text = "element: " + element_choice + " distance: " + dist_val.ToString() + " trajectory: " +  traj_choice;
-        dist_val = distance.value;
-
         element_item = element.value;
         element_choice = element.options[element_item].text;
 
-        traj_item = trajectory.value;
-        traj_choice = trajectory.options[traj_item].text;
+        spell_item = spell_path.value;
+        traj_choice = spell_path.options[spell_item].text;
+
+        range_val = range.value;
+        width_val = width.value;
+        damage_val = damage.value;
+        if (explosion.isOn == true)
+        {
+            explosion_state = true;
+        }
+        else
+        {
+            explosion_state = false;
+        }
+        expl_dmg_val = expl_dmg.value;
+        expl_range_val = expl_range.value;
+
+        
+
+        
+
+        
         
     }
 }
---
+++ Assets/spell_applier.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spell_applier : MonoBehaviour
+{
+    public SpellCombo spell_list;
+    public Material[] element = new Material[4];
+    Renderer m_ObjectRenderer;
+    public Transform orb;
+    public string long_string;
+    public string[] words;
+    public Vector3 scale;
+    private int count = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ObjectRenderer = GetComponent<Renderer>();
+    }
+
+    public void item_seperator()
+    {
+        
+        long_string = spell_list.spell_properties[count];
+        words = long_string.Split(' ');
+
+        
+        //print(spell_list.spell_properties[0]);
+        foreach (var word in words)
+        {
+            print($"<{word}>");
+
+            if (words[0] == "fire")
+            {
+                m_ObjectRenderer.material = element[0];
+            }
+            if (words[0] == "water")
+            {
+                m_ObjectRenderer.material = element[1];
+            }
+            if (words[0] == "air")
+            {
+                m_ObjectRenderer.material = element[2];
+            }
+            if (words[0] == "earth")
+            {
+                m_ObjectRenderer.material = element[3];
+            }
+            scale = new Vector3(float.Parse(words[3]), float.Parse(words[3]), float.Parse(words[3]));
+            orb.localScale = scale;
+        }
+
+        count += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
